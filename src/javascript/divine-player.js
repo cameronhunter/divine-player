0b0ac53a73fd40d94d4fc471f1af266243b1f904
Cameron Hunter <hello@cameronhunter.co.uk>
2013-09-14 19:32:19 -0700

    Simplifying the DivinePlayer entrance point

--- src/javascript/divine-player.js
+++ src/javascript/divine-player.js
@@ -2,13 +2,19 @@ var DivinePlayer = (function() {
 
   var PLAYERS = [HTML5Player, FlashPlayer];
 
-  return {
-    players: PLAYERS,
-    getSupportedPlayer: function() {
-      for (var i=0, l=PLAYERS.length; i<l; i++) {
-        if (PLAYERS[i].canPlay(video)) return PLAYERS[i];
-      }
-    }
-  };
+  function DivinePlayer(el, options, onReady) {
+    var Player = getSupportedPlayer(el);
+    if (!Player) throw "No supported player found.";
+    return new Player(el, options, onReady);
+  }
 
+  DivinePlayer.players = PLAYERS;
+
+  function getSupportedPlayer(video) {
+    for (var i=0, l=PLAYERS.length; i<l; i++) if (PLAYERS[i].canPlay(video)) {
+      return PLAYERS[i];
+    }
+  }
+
+  return DivinePlayer;
 }());
